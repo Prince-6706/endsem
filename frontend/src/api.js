9b3c02d18a3e2a11939d55de25bf2bf5db6755e9
Prince-6706 <prince17sept2004@gmail.com>
2026-05-18 10:38:34 +0530

    Updated frontend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Match backend port
+  baseURL: 'https://endsem-backend.onrender.com/api', // Match backend port
 });
 
 api.interceptors.request.use((config) => {
